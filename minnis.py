08fc0b6b80f606bcda55c9d22545cc70f69b7c82
shhh <shhh9712@gmail.com>
2017-03-11 17:24:17 -0800

    add verify

--- minnis.py
+++ minnis.py
@@ -50,7 +50,8 @@ def main_loop(codes):
             if last_statue != now_statue and i > 1:
                 open_notify(content.pre_name, content.code, content.statue)
             print('{}: {}, is now {}'.format(content.pre_name, content.code, content.statue))
-        time.sleep(5)
+            open_notify(content.pre_name, content.code, content.statue)
+        time.sleep(20)
         
 
         
