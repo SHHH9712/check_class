1f0a613a0bdbd7f8787f9f8379b7d7fcf1daead4
shhh <shhh9712@gmail.com>
2017-03-11 17:16:49 -0800

    fix bug......

--- minnis.py
+++ minnis.py
@@ -50,11 +50,12 @@ def main_loop(codes):
             if last_statue != now_statue and i > 1:
                 open_notify(content.pre_name, content.code, content.statue)
             print('{}: {}, is now {}'.format(content.pre_name, content.code, content.statue))
+            if REPORT_TIME == 10:
+                open_notify(content.pre_name, content.code, content.statue)
+                REPORT_TIME = 0
+            REPORT_TIME += 1
         time.sleep(5)
-        if REPORT_TIME == 10:
-            open_notify(content.pre_name, content.code, content.statue)
-            REPORT_TIME = 0
-        REPORT_TIME += 1
+        
 
         
 if __name__ == '__main__':
